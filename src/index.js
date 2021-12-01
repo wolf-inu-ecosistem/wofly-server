001f413f6b4c6aa5001b0570541a012d4a5d92b8
angeleesc <50447147+angeleesc@users.noreply.github.com>
2021-12-01 13:50:51 -0400

    conecion confirebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import firebaseRoute from './routes/firebaseEcample.js'
 
 const app = express();
 
+app.use('/firebase',firebaseRoute );
 
-app.listen(3000)
+app.listen(3000);
 
-console.log('server listen on port 3000 XD')
+
+console.log('server listen on port 3000 XD');
